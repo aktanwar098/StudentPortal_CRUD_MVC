7faa1081b8301777f599ee23729c771627bd2285
aktanwar098 <ak.tanwar098@gmail.com>
2024-03-06 21:32:04 +0530

    "Update" Student Feature implemented

--- StudentPortal.Web/Controllers/StudentsController.cs
+++ StudentPortal.Web/Controllers/StudentsController.cs
@@ -48,6 +48,37 @@ namespace StudentPortal.Web.Controllers
             var students = await dbContext.Students.ToListAsync();
             return View(students);
         }
+
+
+        //First we get student for Updating student 
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var student = await dbContext.Students.FindAsync(id);
+
+            return View(student);
+        }
+
+        //Now updating student using HttpPost method
+        [HttpPost]
+        public async Task<IActionResult> Edit(Student viewModel)
+        {
+            var student = await dbContext.Students.FindAsync(viewModel.Id);
+
+            if(student is not null)
+            {
+                student.Name = viewModel.Name;
+                student.Email = viewModel.Email;
+                student.Phone = viewModel.Phone;
+                student.Subscribed = viewModel.Subscribed;
+
+                await dbContext.SaveChangesAsync();
+            }
+
+            //now returning to main action called "List"
+
+            return RedirectToAction("List", "Students");
+        }
     }
 
 
